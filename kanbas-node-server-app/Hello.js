cc99a35ce89d69a82ac35d0638badb1ed3ee11f0
ashlynnbraisted <122566610+ashlynnbraisted@users.noreply.github.com>
2024-04-04 18:00:28 -0400

    Delete kanbas-node-server-app directory

--- kanbas-node-server-app/Hello.js
+++
@@ -1,8 +0,0 @@
-export default function Hello(app) {
-    app.get('/hello', (req, res) => {
-    res.send('Life is good!')
-  });
-  app.get('/', (req, res) => {
-    res.send('Welcome to Full Stack Development!')
-  });
-}
